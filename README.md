2ab3ba0b8120bb2bc46819b2bab4713e4510afc5
thuhak <thuhak.zhou@nio.com>
2019-02-15 17:16:18 +0800

    first upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# 一个用于打印客户端请求的web server
+
+
+用于查看某些非自己开发的客户端发出的web请求的内容
+
+## 用法
+
+```cmd
+pip install -r requirments.txt
+python webtrap.py --port=PORT
+```
---
+++ webtrap.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+#author: thuhak.zhou@nio.com
+import sys
+import json
+import tornado.web
+import tornado.ioloop
+from tornado.options import define, options, parse_command_line
+
+
+define('port', default=8888, help='port', type=int)
+parse_command_line()
+
+
+def _tran_to_json(items):
+    if isinstance(items,dict):
+        new_item = {}
+        for k,v in items.items():
+            new_item[k] = _tran_to_json(v)
+        return new_item
+    elif isinstance(items, list):
+        new_item = []
+        for x in items:
+            new_item.append(_tran_to_json(x))
+        return new_item
+    elif isinstance(items, (str, int, float)):
+        return items
+    elif sys.version >= '3' and isinstance(items, bytes):
+        try:
+            data = items.decode('ascii')
+            return data
+        except:
+            return str(items)
+    else:
+        return str(items)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def prepare(self):
+        if self.request.headers.get('Content-Type') == 'application/json':
+            body = json.loads(self.request.body)
+        else:
+            body = self.request.body
+        data = _tran_to_json({'path': self.request.uri,
+                  'method': self.request.method,
+                  'headers': dict(self.request.headers),
+                  'remote_ip': self.request.remote_ip,
+                  'protocol': self.request.protocol,
+                  'arguments': self.request.arguments,
+                  'body': body
+                  })
+        self.ret = json.dumps(data, indent=True)
+        print(self.ret)
+
+    def get(self):
+        self.write(self.ret)
+
+    def post(self):
+        self.write(self.ret)
+
+    def head(self):
+        self.write(self.ret)
+
+    def patch(self):
+        self.write(self.ret)
+
+    def put(self):
+        self.write(self.ret)
+
+
+def make_app():
+    return tornado.web.Application([(r"/.*", MainHandler),])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
